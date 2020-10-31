10b31264883c876adc68a2f6f3689eb9af121229
Anthony Nguyen <thang.nguyen1@fractal.vn>
2020-10-31 17:53:27 +0700

    add ui

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { LazyLoadService, AddReplaceableComponent } from '@abp/ng.core';
+import { eThemeBasicComponents } from '@abp/ng.theme.basic';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { LoginComponent } from './login/login.component';
+import { eAccountComponents } from '@abp/ng.account';
+import { OuLayoutComponent } from './shared/layout/app-layout/ou-layout.component';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <abp-loader-bar></abp-loader-bar>
-    <abp-dynamic-layout></abp-dynamic-layout>
-  `,
+  templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private lazyLoadService: LazyLoadService, private store: Store) { }
+
+  ngOnInit() {
+    this.store.dispatch(
+      new AddReplaceableComponent({
+        component: OuLayoutComponent,
+        key: eThemeBasicComponents.ApplicationLayout,
+      })
+    );
+
+    this.store.dispatch(
+      new AddReplaceableComponent({
+        component: LoginComponent,
+        key: eAccountComponents.Login
+      })
+    );
+  }
+}
+
---
+++ angular/src/app/shared/layout/account/account-layout-component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'account-layout',
+  templateUrl: 'account-layout.component.html'
+})
+
+export class OuAccountLayoutComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
